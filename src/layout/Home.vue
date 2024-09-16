f2e084134fce941c610db71bc33569a7cadb46ea
Pierpaolo <pierpaolo.paio@edu.unife.it>
2024-09-16 20:00:29 +0200

    modificato homepage

--- src/layout/Home.vue
+++ src/layout/Home.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="container">
-    <h1 class="text-center">Welcome to our app</h1>
-    <p style="margin-top: 50px; font-size: 25px;">
-      This is the homepage. Use the sidebar to navigate.
+    <h1 class="text-center">Welcome to the CSV Data Viewer</h1>
+    <p style="margin-top: 30px; font-size: 18px;">
+      This application allows you to load and view data from CSV files quickly and easily. <br>
+      You can upload your files, view the data in tabular format, and generate graphic graphics for quick analysis.
+    </p>
+    <p style="margin-top: 20px; font-size: 18px;">
+      Use the sidebar to navigate between the different sections:
+      <ul>
+        <li><strong>Upload CSV</strong>: Upload your CSV files.</li>
+        <li><strong>Path Input</strong>: Enter the path to upload a CSV file directly from your system.</li>
+      </ul>
+    </p>
+    <p style="margin-top: 30px; font-size: 20px;">
+      Ready to start? Upload your first CSV file using the navigation menu!
     </p>
   </div>
 </template>
-  
+
 <script>
 export default {
   name: 'TheHome',
